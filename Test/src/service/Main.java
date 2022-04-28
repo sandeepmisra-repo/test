3ea006681e19fcb40fc9ae25569ed7ffa00c8c8d
sandeepmisra-repo <sandeep.misra96@gmail.com>
2022-04-28 16:45:23 +0530

    New Branch

--- Test/src/service/Main.java
+++ Test/src/service/Main.java
@@ -1,8 +1,17 @@
 package service;
 
 public class Main {
+	public static String msg(String _msg) {
+		String res = "";
+		try {
+			res = _msg;
+		} catch (Exception ex) {
+			System.out.println(ex.toString());
+		}
+		return res;
+	}
 
 	public static void main(String[] args) {
-		System.out.println("This is test");
+		System.out.println(msg("This is test"));
 	}
 }
